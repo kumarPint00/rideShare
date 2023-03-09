0e1372a821976432f479235e24eab5165af60b55
KumarPint00 <kumarpintoo417@gmail.com>
2023-03-09 23:31:22 +0530

    sidebar

--- ride-share/src/index.js
+++ ride-share/src/index.js
@@ -1,16 +1,18 @@
-import { Link } from '@mui/material';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import Navbar from './Components/Navbar'
-import JoinRide from './Components/JoinRide'
-import StartRide from './Components/StartRide';
+
+import Sidebar from './Components/Sidebar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Navbar/>
+
+    <Sidebar/>
     <App />
+    {/* <Navbar/> */}
   </React.StrictMode>
 );
 
